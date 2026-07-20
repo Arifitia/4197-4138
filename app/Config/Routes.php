3927ec894cfa26ab64bb2a546bc3a0c700c2c2ce
Arifitia <fitiathebest@gmail.com>
2026-07-20 10:45:09 +0300

    login, session, dashbord

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -4,3 +4,8 @@ use CodeIgniter\Router\RouteCollection;
 
 /** @var RouteCollection $routes */
 $routes->get('/', 'Home::index');
+$routes->get('auth', 'AuthController::index');
+$routes->post('auth/login', 'AuthController::login');
+$routes->get('dashboard', 'DashboardController::index');
+$routes->get('dashboard/logout', 'DashboardController::logout');
+
